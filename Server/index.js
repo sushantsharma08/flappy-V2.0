a281f98b53d6d64a5ce185d8dec1ce3af33160e9
Sushant Sharma <ssushantlb08@gmail.com>
2023-04-17 01:09:40 +0530

    environment vars added

--- Server/index.js
+++ Server/index.js
@@ -3,18 +3,22 @@ import cors from 'cors';
 import {UserRouter} from './src/routes/user.js'
 import mongoose from 'mongoose';
 import { ScoreRouter } from "./src/routes/score.js";
+import * as dotenv from "dotenv";
 
 const app= express();
+dotenv.config({path:"./config.env"});
+
 app.use(express.json());
 app.use(cors());
 
+const port =process.env.PORT
 
 app.use('/auth',UserRouter);
 app.use('/score',ScoreRouter);
 
-mongoose.connect("mongodb+srv://sushantsharmadev:flappy2@flappy2-0.sua2p9z.mongodb.net/flappy2-0?retryWrites=true&w=majority")
+mongoose.connect(`mongodb+srv://${process.env.USERNAME_DB}:${process.env.PASSWORD_DB}@flappy2-0.sua2p9z.mongodb.net/flappy2-0?retryWrites=true&w=majority`)
 
 
-app.listen(3001,()=>{
+app.listen(port,()=>{
     console.log('Server up!');
 })
